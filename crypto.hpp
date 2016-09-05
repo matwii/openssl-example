f27c5f52c405d6897e9403d14f8481531f8acc9f
Matz Wiik <matzwiik@gmail.com>
2016-09-05 15:18:14 +0200

    Added sha-512

--- crypto.hpp
+++ crypto.hpp
@@ -52,7 +52,11 @@ public:
 
   /// Return the SHA-512 (512-bit) hash from input.
   static std::string sha512(const std::string &input, size_t iterations = 1) {
-    throw std::logic_error("not yet implemented");
+    std::string hash;
+    hash.resize(512 / 8);
+    SHA512((const unsigned char *)input.c_str(), input.size(), (unsigned char *)hash.c_str());
+
+    return hash;
   }
 
   /// Return key from the Password-Based Key Derivation Function 2 (PBKDF2).
--- examples.cpp
+++ examples.cpp
@@ -16,6 +16,9 @@ int main() {
   
   cout << "SHA-256 with 1 iteration" << endl;
   cout << Crypto::hex(Crypto::sha256("Test")) << endl << endl;
+  
+  cout << "SHA-512 with 1 iteration" << endl;
+  cout << Crypto::hex(Crypto::sha512("Test")) << endl << endl;
 
   cout << "The derived key from the PBKDF2 algorithm" << endl;
   cout << Crypto::hex(Crypto::pbkdf2("Password", "Salt")) << endl;
